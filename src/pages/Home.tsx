81d735a131f8316352502294ad8bf47bc129e896
Roberto <beto.rodriguesds@gmail.com>
2022-08-15 15:40:40 -0300

    feat: add pagination and request to api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,58 +1,75 @@
 import React, { useEffect, useState } from 'react'
 import { listItems } from '../services/list'
 import { Box, Flex } from '@chakra-ui/react'
-import { useSorting } from '../utils/sort'
 import Table from '../components/Table'
-import { ListMetadata } from '../@types/generic'
+import { ListMetadata, PaginationPayload } from '../@types/generic'
+import useSafePromise from '../hooks/useSafePromise'
+
+interface dataProps {
+  count: number
+  results: [{ name: string; url: string }]
+}
 
 function Home() {
-  const [data, setData] = useState<[{ name: string }]>()
-
-  const metadata = {}
+  const [data, setData] = useState<dataProps>()
+  const { safe, isLoading, isEmpty } = useSafePromise()
+  const [metadata, setMetadata] = useState<PaginationPayload>({
+    page: 1,
+    pageSize: 10
+  })
 
   useEffect(() => {
     async function getData() {
-      try {
-        const { data } = await listItems()
-        setData(data.results)
-      } catch (error) {
-        console.log(error)
-      }
+      await safe(listItems(metadata.page, metadata.pageSize), setData)
     }
     getData()
   }, [])
 
-  const handleMetadata = (newMetadata: Partial<ListMetadata>) => {
-    console.log('metadata')
+  useEffect(() => {
+    async function setNewMetadata() {
+      setMetadata({ ...metadata, totalItems: data?.count })
+    }
+    setNewMetadata()
+  }, [data])
+
+  const handleMetadata = async (newMetadata: Partial<ListMetadata>) => {
+    setMetadata({ ...metadata, ...newMetadata })
+    await safe(listItems(metadata.page, metadata.pageSize), setData)
   }
 
-  const columns = useSorting(
-    [
-      {
-        header: 'Nome',
-        accessor: 'name'
-      }
-    ],
-    metadata
-  )
+  const columns = [
+    {
+      header: 'Nome',
+      accessor: 'name'
+    },
+    {
+      header: 'URL',
+      accessor: 'url'
+    }
+  ]
 
   const rows =
-    data?.map((item) => ({
+    data?.results.map((item) => ({
       cells: [
         {
           field: 'name',
           value: item.name
+        },
+        {
+          field: 'url',
+          value: item.url
         }
       ]
     })) || []
 
   return (
-    <Flex align={'center'} justify={'center'}>
-      <Box boxShadow={'md'}>
+    <Flex align={'center'} justify={'center'} p={10}>
+      <Box boxShadow={'xl'} w={'full'} p={4}>
         <Table
           headerColumns={columns}
           rows={rows}
-          isLoading={!data}
+          isLoading={isLoading}
+          isEmpty={isEmpty}
           metadata={metadata}
           handleSort={handleMetadata}
         />
